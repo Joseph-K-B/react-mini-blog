8af69835fcbd8413f8241ff9091a1c9f8d9f30dd
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-11-29 14:18:32 -0800

    state for title and font

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,18 +1,35 @@
+/* eslint-disable prettier/prettier */
 import React from 'react'
 
 import Preview from '../../components/Preview/Preview'
 import Editor from '../../components/Editor/Editor'
+import { useState } from 'react'
 
 import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+  const [title, setTitle] = useState('')
+  const [font, setFont] = useState('')
+
+  
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview 
+        title={title}
+        setTitle={setTitle}
+        font={font}
+        setFont={setFont}
+      />
+      <Editor
+        title={title}
+        setTitle={setTitle}
+        font={font}
+        setFont={setFont}
+      />
     </main>
   )
+  // eslint-disable-next-line prettier/prettier
 }
